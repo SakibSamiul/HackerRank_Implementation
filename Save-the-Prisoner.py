6e0718b742eeb41e43ae3eacad3adc14de66cc34
SakibSamiul <sb.sakib77@gmail.com>
2024-09-01 14:30:03 +0600

    save the Prisoner

---
+++ Save-the-Prisoner.py
@@ -0,0 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
+
+def saveThePrisoner(n, m, s):
+
+    print(((s - 1 + m - 1) % n) + 1)
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        s = int(first_multiple_input[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        # fptr.write(str(result) + '\n')
+
+    # fptr.close()
