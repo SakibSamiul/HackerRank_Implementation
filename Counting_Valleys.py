1bc6cd714ce31c5c8c76249443f351917cf3f44b
SakibSamiul <sb.sakib77@gmail.com>
2024-10-02 19:08:06 +0600

    Between Two Sets

--- Counting_Valleys.py
+++ Counting_Valleys.py
@@ -1,4 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
 import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
 
 def countingValleys(n, steps):
     seaLevel = valley = 0
@@ -14,7 +29,16 @@ def countingValleys(n, steps):
     
     return valley
 
+
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     steps = int(input().strip())
+
     path = input()
-    countingValleys(steps, path)
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
